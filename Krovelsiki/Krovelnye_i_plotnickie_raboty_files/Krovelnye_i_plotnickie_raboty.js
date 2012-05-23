de40e59163aa82c94d23d5be389de1df7da74489
roofs <vasili.kiselev@gmail.com>
2012-05-24 00:28:16 +0400

    GA manual
    <script type="text/javascript">
    
      var _gaq = _gaq || [];
      _gaq.push(['_setAccount', 'UA-32057404-1']);
      _gaq.push(['_trackPageview']);
    
      (function() {
        var ga = document.createElement('script'); ga.type =
    'text/javascript'; ga.async = true;
        ga.src = ('https:' == document.location.protocol ? 'https://ssl' :
    'http://www') + '.google-analytics.com/ga.js';
        var s = document.getElementsByTagName('script')[0];
    s.parentNode.insertBefore(ga, s);
      })();
    
    </script>

--- Krovelsiki/Krovelnye_i_plotnickie_raboty_files/Krovelnye_i_plotnickie_raboty.js
+++ Krovelsiki/Krovelnye_i_plotnickie_raboty_files/Krovelnye_i_plotnickie_raboty.js
@@ -1,11 +1,11 @@
-// Created by iWeb 3.0.4 local-build-20120523
+// Created by iWeb 3.0.4 local-build-20120524
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke(),reflection_0:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:1.00}),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Krovelnye_i_plotnickie_raboty_files/Krovelnye_i_plotnickie_rabotyMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');detectBrowser();Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
