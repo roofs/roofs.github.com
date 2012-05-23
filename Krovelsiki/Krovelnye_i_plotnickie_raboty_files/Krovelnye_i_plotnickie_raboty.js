8c192a8f6dcefe22dc1e14f020bec8bb2ad212d0
roofs <vasili.kiselev@gmail.com>
2012-05-23 23:36:56 +0400

    Revert "4 edition GAn"
    This reverts commit cb66459802178425099c8e29d5d660b6026d1a0d.

--- Krovelsiki/Krovelnye_i_plotnickie_raboty_files/Krovelnye_i_plotnickie_raboty.js
+++ Krovelsiki/Krovelnye_i_plotnickie_raboty_files/Krovelnye_i_plotnickie_raboty.js
@@ -6,6 +6,6 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Krovelnye_i_plotnickie_raboty_files/Krovelnye_i_plotnickie_rabotyMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');detectBrowser();Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
